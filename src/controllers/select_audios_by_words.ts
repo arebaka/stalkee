36d1b60511e14d0bf589abcd98dec0c8e55bbb86
arelive <me@are.moe>
2024-03-10 00:03:05 +0300

    optimize: searching by words

--- src/controllers/select_audios_by_words.ts
+++ src/controllers/select_audios_by_words.ts
@@ -3,27 +3,48 @@ import { Audio } from '../models'
 
 export const selectAudiosByWords = async (words: string[]): Promise<Audio[]|undefined> => {
 	try {
-		const audios = await Audio.find({
-			order: { nUses: 'DESC' },
-			relations: {
-				words: true
-			}
-		})
+		if (words.length == 0) {
+			return []
+		}
 
-		for (const word of words) {
-			for (let i = 0; i < audios.length; i++) {
-				const index = audios[i].words.map(w => w.word).indexOf(word)
+		// limitation: select all audios that include first word
+		const audios = await Audio.createQueryBuilder('a')
+			.innerJoin('a.words', 'w1')
+			.where('w1.word = :word', { word: words[0] })
+			.innerJoinAndSelect('a.words', 'w2')
+			.orderBy('a.n_uses', 'DESC')
+			.getMany()
 
-				if (index < 0) {
-					audios.splice(i--, 1)
-				} else {
-					audios[i].words.splice(index, 1)
+		// count same words of each selected audio, sync indexes
+		const wordsCounter: {[word: string]: number}[] = []
+		for (const audio of audios) {
+			const counter: {[word: string]: number} = {}
+			for (const word of audio.words) {
+				if (!(word.word in counter)) {
+					counter[word.word] = 0
 				}
+				counter[word.word]++
+			}
+			wordsCounter.push(counter)
+		}
+
+		const res: Audio[] = []
+		for (let i = 0; i < audios.length; i++) {
+			let excluded = false
+			for (const word of words) {
+				if (!(word in wordsCounter[i]) || wordsCounter[i][word] <= 0) {
+					excluded = true
+					break
+				}
+				wordsCounter[i][word]--
+			}
+			if (!excluded) {
+				res.push(audios[i])
 			}
 		}
 
-		audios.splice(config.limits.max_result_size)
-		return audios
+		res.splice(config.limits.max_result_size)
+		return res
 	}
 	catch (err) {
 		logger.error(''+err, 'controller.select_audios_by_words')
